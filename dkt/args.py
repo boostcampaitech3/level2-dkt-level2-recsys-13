f78754e4ef4c7ebaaea244985ab6d071e91105bd
SNMHZ <snmhz325@gmail.com>
2022-05-12 07:40:16 +0000

    feat: Add scheduler `CosineAnnealingWarmUpRestarts`

--- dkt/args.py
+++ dkt/args.py
@@ -37,27 +37,27 @@ def parse_args(mode="train"):
     )
 
     parser.add_argument(
-        "--max_seq_len", default=300, type=int, help="max sequence length"
+        "--max_seq_len", default=250, type=int, help="max sequence length"
     )
-    parser.add_argument("--num_workers", default=1, type=int, help="number of workers")
+    parser.add_argument("--num_workers", default=8, type=int, help="number of workers")
 
     # 모델
     parser.add_argument(
-        "--hidden_dim", default=2048, type=int, help="hidden dimension size"
+        "--hidden_dim", default=256, type=int, help="hidden dimension size"
     )
     parser.add_argument("--n_layers", default=2, type=int, help="number of layers")
     parser.add_argument("--n_heads", default=4, type=int, help="number of heads")
     parser.add_argument("--drop_out", default=0.1, type=float, help="drop out rate")
 
     # 훈련
-    parser.add_argument("--n_epochs", default=20, type=int, help="number of epochs")
+    parser.add_argument("--n_epochs", default=50, type=int, help="number of epochs")
     parser.add_argument("--batch_size", default=64, type=int, help="batch size")
-    parser.add_argument("--lr", default=0.0001, type=float, help="learning rate")
+    parser.add_argument("--lr", default=0.001, type=float, help="learning rate")
     parser.add_argument("--clip_grad", default=0, type=int, help="clip grad")
     parser.add_argument("--patience", default=5, type=int, help="for early stopping")
 
     parser.add_argument("--window", default=True, type=bool, help="for augmentation window")
-    parser.add_argument("--stride", default=250, type=int, help="for augmentation stride")
+    parser.add_argument("--stride", default=200, type=int, help="for augmentation stride")
     parser.add_argument("--shuffle", default=False, type=bool, help="for augmentation shuffle")
     parser.add_argument("--shuffle_n", default=2, type=int, help="for augmentation shuffle n")
 
@@ -69,9 +69,9 @@ def parse_args(mode="train"):
 
     ### 중요 ###
     parser.add_argument("--model", default="lstmattn", type=str, help="model type")
-    parser.add_argument("--optimizer", default="adamW", type=str, help="optimizer type")
+    parser.add_argument("--optimizer", default="adam", type=str, help="optimizer type")
     parser.add_argument(
-        "--scheduler", default="plateau", type=str, help="scheduler type"
+        "--scheduler", default="CosineAnnealingWarmUpRestarts", type=str, help="scheduler type"
     )
 
     ### 추가 ###
